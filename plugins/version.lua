b557ee0f52f8b5da34ed273a55dda7dcc5654e8d
makanishere <makan.j8080@gmail.com>
2016-02-29 14:57:29 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,8 +14,8 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot \n\nhttp://github.com/Josepdal/DBTeam\n\nDevelopers :\n@MaSkAoS\n@Josepdal\n\nSupport :\n@skneos\n@iicc1\n@Serx666' 
-  --[[ Checkout https://github.com/Josepdal/DBTeam
+  return 'CycloneTG Bot V5 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot' 
+  --[[ Checkout https://github.com/makanishere/CycloneTG
   --GNU GPL v2 license.]]
 end
 
